ebb51ec5858ff73170e671b8e89dda7f7c461ad6
Dan Train <dan.train@red-badger.com>
2024-01-31 14:05:30 +0000

    Improve Progress component

--- packages/ui/src/Progress/Progress.tsx
+++ packages/ui/src/Progress/Progress.tsx
@@ -1,50 +1,33 @@
 import { useNProgress } from "@tanem/react-nprogress";
-import { useEffect, useState } from "react";
+import {
+  RefObject,
+  createRef,
+  forwardRef,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import {
+  Transition,
+  TransitionGroup,
+  TransitionStatus,
+} from "react-transition-group";
 
-export function Progress({ isLoading }: { isLoading: boolean }) {
-  const [isAnimating, setIsAnimating] = useState(false);
-  const [isVisible, setIsVisible] = useState(false);
+const Bar = forwardRef<HTMLDivElement, { state: TransitionStatus }>(
+  ({ state }, ref) => {
+    const { animationDuration, isFinished, progress } = useNProgress({
+      isAnimating: state === "entered",
+    });
 
-  const { animationDuration, isFinished, progress } = useNProgress({
-    isAnimating,
-  });
-
-  useEffect(() => {
-    let timeout: NodeJS.Timeout | undefined;
-
-    if (isLoading) {
-      clearTimeout(timeout);
-
-      timeout = setTimeout(() => {
-        setIsAnimating(true);
-        setIsVisible(true);
-      }, 500);
-    } else {
-      setIsAnimating(false);
-
-      timeout = setTimeout(() => {
-        setIsVisible(false);
-      }, animationDuration * 2);
-    }
-
-    return () => clearTimeout(timeout);
-  }, [animationDuration, isLoading]);
-
-  return (
-    <div
-      className="pointer-events-none fixed inset-x-0 top-0 h-1 overflow-hidden"
-      style={{
-        opacity: isFinished ? 0 : 1,
-        transition: `opacity ${animationDuration}ms linear`,
-      }}
-    >
+    return (
       <div
         className="absolute left-0 z-50 h-[2px] w-full"
+        ref={ref}
         style={{
-          marginLeft: `${isVisible ? (-1 + progress) * 100 : -100}%`,
-          transition: `margin-left ${
-            isAnimating ? animationDuration : 0
-          }ms linear`,
+          marginLeft: `${(-1 + progress) * 100}%`,
+          opacity:
+            (state === "exiting" || state === "exited") && isFinished ? 0 : 1,
+          transition: `all ${animationDuration}ms linear`,
         }}
       >
         <div
@@ -55,6 +38,45 @@ export function Progress({ isLoading }: { isLoading: boolean }) {
         />
         <div className="relative z-10 h-full w-full bg-white" />
       </div>
-    </div>
+    );
+  },
+);
+
+Bar.displayName = "Bar";
+
+export function Progress({ isLoading }: { isLoading: boolean }) {
+  const [bar, setBar] = useState<{
+    key: number;
+    nodeRef: RefObject<HTMLDivElement>;
+  } | null>(null);
+
+  const isLoadingRef = useRef(isLoading);
+  const keyRef = useRef(0);
+
+  useEffect(() => {
+    if (isLoading && !isLoadingRef.current) {
+      keyRef.current++;
+      setBar({ key: keyRef.current, nodeRef: createRef() });
+    } else if (!isLoading && isLoadingRef.current) {
+      setBar(null);
+    }
+
+    isLoadingRef.current = isLoading;
+  }, [isLoading]);
+
+  return (
+    <TransitionGroup className="pointer-events-none fixed inset-x-0 top-0 h-1 overflow-hidden">
+      {bar ? (
+        <Transition
+          key={bar.key}
+          mountOnEnter
+          nodeRef={bar.nodeRef}
+          timeout={500}
+          unmountOnExit
+        >
+          {(state) => <Bar ref={bar.nodeRef} state={state} />}
+        </Transition>
+      ) : null}
+    </TransitionGroup>
   );
 }
