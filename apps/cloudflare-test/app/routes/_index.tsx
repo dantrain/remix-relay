56787143c19d23c56a99cdc3f6e7db4821e5c89d
Dan Train <dan.train@red-badger.com>
2024-04-13 17:43:35 +0100

    Add cloudflare-test app

---
+++ apps/cloudflare-test/app/routes/_index.tsx
@@ -0,0 +1,29 @@
+import type { MetaFunction } from "@remix-run/cloudflare";
+
+export const meta: MetaFunction = () => {
+  return [{ title: "Cloudflare Test" }];
+};
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <h1>Welcome to Remix (with Vite and Cloudflare)</h1>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/"
+            rel="noreferrer"
+          >
+            Cloudflare Pages Docs - Remix guide
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
