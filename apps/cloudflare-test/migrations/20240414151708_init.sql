cb8016342e7fef5eb8c8a352666d3a6db6232826
Dan Train <dan.train@red-badger.com>
2024-04-14 15:37:54 +0100

    Add migrate scripts

---
+++ apps/cloudflare-test/migrations/20240414151708_init.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `test` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`title` text NOT NULL
+);
---
+++ apps/cloudflare-test/scripts/migrate-drizzle.mjs
@@ -0,0 +1,37 @@
+#!/usr/bin/env zx
+/* eslint-disable turbo/no-undeclared-env-vars */
+import { $, question, fs, path } from "zx";
+import { snakeCase } from "lodash-es";
+import { format } from "date-fns";
+import invariant from "tiny-invariant";
+
+process.env.FORCE_COLOR = "1";
+
+const name = snakeCase(await question("Migration name: "));
+const newFilename = `${format(new Date(), "yyyyMMddHHmmss")}_${name}`;
+
+const result = await $`drizzle-kit generate:sqlite`.pipe(process.stdout);
+const lastLine = result.stdout.split("\n").filter(Boolean).pop();
+
+if (lastLine?.includes("Your SQL migration file")) {
+  const oldFilename = /(?<=migrations\/)[^.]+/.exec(lastLine)?.[0];
+
+  invariant(oldFilename);
+
+  await fs.rename(
+    path.join(import.meta.dirname, `../migrations/${oldFilename}.sql`),
+    path.join(import.meta.dirname, `../migrations/${newFilename}.sql`),
+  );
+
+  const journalPath = path.join(
+    import.meta.dirname,
+    "../migrations/meta/_journal.json",
+  );
+
+  const journal = await fs.readFile(journalPath, "utf-8");
+
+  await fs.writeFile(journalPath, journal.replace(oldFilename, newFilename));
+
+  console.log();
+  console.log(`Wrote ${newFilename}.sql`);
+}
