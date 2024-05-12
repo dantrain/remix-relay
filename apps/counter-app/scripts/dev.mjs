e97ad53398b11cef5a182224e47442fd33e048e7
Dan Train <dan.train@red-badger.com>
2024-05-12 13:13:33 +0100

    Pipe counter app dev output to stdout

--- apps/counter-app/scripts/dev.mjs
+++ apps/counter-app/scripts/dev.mjs
@@ -13,6 +13,8 @@ process.on("uncaughtException", () => {
   process.exit(0);
 });
 
-$`NODE_ENV=development node --import tsx/esm --watch-path=./server ./server/index.ts`;
+$`NODE_ENV=development node --import tsx/esm --watch-path=./server ./server/index.ts`.pipe(
+  process.stdout,
+);
 $`watchman-make -p 'server/graphql/**/*.ts' --run 'pnpm write-graphql-schema'`;
 $`relay-compiler --watch`;
