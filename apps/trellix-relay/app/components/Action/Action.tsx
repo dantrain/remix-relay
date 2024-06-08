4486f6c4b3e483bbdfbb6ca6f08ab8af8b5a5f7a
Dan Train <dan.train@red-badger.com>
2024-06-08 17:56:58 +0100

    Remove CSS modules

--- apps/trellix-relay/app/components/Action/Action.tsx
+++ apps/trellix-relay/app/components/Action/Action.tsx
@@ -1,6 +1,4 @@
-import { cx } from "class-variance-authority";
 import { forwardRef, CSSProperties, HTMLAttributes } from "react";
-import styles from "./Action.module.css";
 
 export type ActionProps = HTMLAttributes<HTMLButtonElement> & {
   active?: {
@@ -11,14 +9,13 @@ export type ActionProps = HTMLAttributes<HTMLButtonElement> & {
 };
 
 export const Action = forwardRef<HTMLButtonElement, ActionProps>(
-  ({ active, className, cursor, style, ...props }, ref) => {
+  ({ active, cursor, style, ...props }, ref) => {
     return (
       <button
         ref={ref}
         type="button"
         {...props}
         aria-describedby={undefined}
-        className={cx(styles.Action, className)}
         tabIndex={0}
         style={
           {
