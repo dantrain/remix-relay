f874d5bd1311c96bd77cd5d0ac54795f9811470f
Dan Train <dan.train@red-badger.com>
2024-06-17 14:49:04 +0100

    Add error and 404 page

--- apps/trellix-relay/app/root.tsx
+++ apps/trellix-relay/app/root.tsx
@@ -4,8 +4,11 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  isRouteErrorResponse,
+  useRouteError,
 } from "@remix-run/react";
-import { Suspense } from "react";
+import { TriangleAlertIcon } from "lucide-react";
+import { ReactNode, Suspense, useEffect } from "react";
 import { RelayEnvironmentProvider } from "react-relay";
 import { RemixRelayProvider } from "@remix-relay/react";
 import { Spinner, Toaster } from "@remix-relay/ui";
@@ -14,7 +17,7 @@ import Progress from "./components/Progress";
 import { ResubscribeProvider } from "./components/ResubscribeProvider";
 import "./tailwind.css";
 
-export default function App() {
+export function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
@@ -25,20 +28,63 @@ export default function App() {
         <Links />
       </head>
       <body className="bg-slate-200 text-slate-950">
-        <RemixRelayProvider>
-          <RelayEnvironmentProvider environment={getCurrentEnvironment()}>
-            <ResubscribeProvider>
-              <Progress />
-              <Suspense fallback={<Spinner className="h-36" />}>
-                <Outlet />
-              </Suspense>
-              <Toaster variant="light" />
-            </ResubscribeProvider>
-          </RelayEnvironmentProvider>
-        </RemixRelayProvider>
+        {children}
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
+  );
+}
+
+export default function App() {
+  return (
+    <RemixRelayProvider>
+      <RelayEnvironmentProvider environment={getCurrentEnvironment()}>
+        <ResubscribeProvider>
+          <Progress />
+          <Suspense fallback={<Spinner className="h-36" />}>
+            <Outlet />
+          </Suspense>
+          <Toaster variant="light" />
+        </ResubscribeProvider>
+      </RelayEnvironmentProvider>
+    </RemixRelayProvider>
+  );
+}
+export function ErrorBoundary() {
+  const error = useRouteError();
+  const isRouteError = isRouteErrorResponse(error);
+
+  const message = isRouteError
+    ? `${error.status} ${error.statusText}`
+    : error instanceof Error
+      ? error.message
+      : "Unknown Error";
+
+  const heading = isRouteError ? "Page not found" : "Something went wrong";
+
+  useEffect(() => {
+    document.title = `${heading} | Trellix Relay`;
+  });
+
+  return (
+    <main
+      className="grid min-h-[100dvh]
+        grid-cols-[minmax(1rem,1fr)_auto_minmax(1rem,1fr)]
+        grid-rows-[minmax(1rem,1fr)_auto_minmax(1rem,3fr)] justify-center"
+    >
+      <div
+        className="col-start-2 row-start-2 grid max-w-[768px] gap-6 rounded-md
+          border border-slate-300 bg-white px-4 py-6 shadow-sm sm:p-8"
+      >
+        <h1 className="flex items-center justify-center gap-2 text-2xl">
+          <TriangleAlertIcon className="text-red-700" />
+          {heading}
+        </h1>
+        <pre className="overflow-auto rounded-sm bg-slate-200 p-2 text-center">
+          {message}
+        </pre>
+      </div>
+    </main>
   );
 }
