acd8e345b56765f13309b5b0fe8bc647f617f81c
Dan Train <dan.train@red-badger.com>
2024-07-10 10:53:57 +0100

    Batch transaction statements

--- apps/trellix-relay/server/drizzle-client.ts
+++ apps/trellix-relay/server/drizzle-client.ts
@@ -15,20 +15,18 @@ export function getDb(session: Session) {
 
     return db.transaction(async (tx) => {
       // Set JWT to enable RLS. supabase adds the role and the userId (sub) to the jwt claims
-      await tx.execute(
-        sql`SELECT set_config('request.jwt.claims', '${sql.raw(JSON.stringify(claims))}', TRUE)`,
-      );
-
-      // do not use postgres because it will bypass the RLS, set role to authenticated
-      await tx.execute(sql`set role '${sql.raw(claims.role)}'`);
+      // Do not use postgres role because it will bypass the RLS, set role to authenticated
+      await tx.execute(sql`
+        select set_config('request.jwt.claims', '${sql.raw(JSON.stringify(claims))}', true);
+        set role '${sql.raw(claims.role)}';
+      `);
 
       const result = await cb(tx);
 
-      await tx.execute(
-        sql`SELECT set_config('request.jwt.claims', NULL, true);`,
-      );
-
-      await tx.execute(sql`RESET ROLE;`);
+      await tx.execute(sql`
+        select set_config('request.jwt.claims', null, true);
+        reset role;
+      `);
 
       return result;
     }) as Promise<T>;
