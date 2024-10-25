1b93c3538e7a9741a307fa58832832516d67cc4f
Dan Train <dan.train@red-badger.com>
2024-10-25 20:17:36 +0100

    Use v3_singleFetch

--- apps/trellix-relay/vite.config.ts
+++ apps/trellix-relay/vite.config.ts
@@ -1,11 +1,8 @@
 import { vitePlugin as remix } from "@remix-run/dev";
-import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import { cjsInterop } from "vite-plugin-cjs-interop";
 import relay from "vite-plugin-relay";
 import tsconfigPaths from "vite-tsconfig-paths";
-
-installGlobals();
 
 export default defineConfig(({ command }) => ({
   plugins: [
@@ -17,6 +14,9 @@ export default defineConfig(({ command }) => ({
     remix({
       ignoredRouteFiles: ["**/.*"],
       serverBuildFile: "remix.js",
+      future: {
+        v3_singleFetch: true,
+      },
     }),
   ],
   ssr: command === "build" ? { noExternal: /(relay)/ } : {},
