95415a3ac44ed9e2d0f52ab2406a6980e88cf72a
Dan Train <dan.train@red-badger.com>
2024-10-30 17:58:23 +0000

    Update Drizzle Client based on Drizzle docs example

--- apps/trellix-relay/server/drizzle-client.ts
+++ apps/trellix-relay/server/drizzle-client.ts
@@ -11,24 +11,28 @@ const db = drizzle(client, { schema, casing: "snake_case" });
 
 export function getDb(session: Session) {
   return <T>(cb: (tx: PostgresJsDatabase<typeof schema>) => T | Promise<T>) => {
-    const claims = jwtDecode<{ role: string }>(session.access_token);
+    const claims = jwtDecode<{ role?: string; sub?: string }>(
+      session.access_token,
+    );
 
     return db.transaction(async (tx) => {
-      // Set JWT to enable RLS. supabase adds the role and the userId (sub) to the jwt claims
-      // Do not use postgres role because it will bypass the RLS, set role to authenticated
-      await tx.execute(sql`
-        select set_config('request.jwt.claims', '${sql.raw(JSON.stringify(claims))}', true);
-        set role '${sql.raw(claims.role)}';
-      `);
+      try {
+        // Set JWT to enable RLS. supabase adds the role and the userId (sub) to the jwt claims
+        // Do not use postgres role because it will bypass the RLS, set role to authenticated
+        await tx.execute(sql`
+          select set_config('request.jwt.claims', '${sql.raw(JSON.stringify(claims))}', true);
+          select set_config('request.jwt.claim.sub', '${sql.raw(claims.sub ?? "")}', true);
+          set local role '${sql.raw(claims.role ?? "anon")}';
+        `);
 
-      const result = await cb(tx);
-
-      tx.execute(sql`
-        select set_config('request.jwt.claims', null, true);
-        reset role;
-      `);
-
-      return result;
+        return await cb(tx);
+      } finally {
+        await tx.execute(sql`
+          select set_config('request.jwt.claims', null, true);
+          select set_config('request.jwt.claim.sub', null, true);
+          reset role;
+        `);
+      }
     }) as Promise<T>;
   };
 }
