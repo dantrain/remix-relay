55441dadafd2023b3246f30923b481f74b3c0af0
Dan Train <dan.train@red-badger.com>
2024-12-08 12:00:19 +0000

    Remove forwardRef

--- apps/trellix-relay/app/components/DroppableColumn.tsx
+++ apps/trellix-relay/app/components/DroppableColumn.tsx
@@ -1,63 +1,69 @@
 import { UniqueIdentifier } from "@dnd-kit/core";
 import { defaultAnimateLayoutChanges, useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
-import { forwardRef } from "react";
+import { Ref } from "react";
 import { Column, ColumnProps } from "./Column";
 
 export type DroppableColumnProps = ColumnProps & {
   id: UniqueIdentifier;
   items?: { id: UniqueIdentifier }[];
+  ref?: Ref<HTMLDivElement>;
 };
 
-export const DroppableColumn = forwardRef<HTMLDivElement, DroppableColumnProps>(
-  ({ children, id, items = [], ...props }, ref) => {
-    const {
-      active,
-      attributes,
-      isDragging,
-      listeners,
-      over,
-      setNodeRef,
-      transition,
-      transform,
-    } = useSortable({
-      id,
-      data: {
-        type: "container",
-        children: items,
-      },
-      animateLayoutChanges: (args) =>
-        defaultAnimateLayoutChanges({ ...args, wasDragging: true }),
-    });
+export function DroppableColumn({
+  children,
+  id,
+  items = [],
+  ref,
+  ...props
+}: DroppableColumnProps) {
+  const {
+    active,
+    attributes,
+    isDragging,
+    listeners,
+    over,
+    setNodeRef,
+    transition,
+    transform,
+  } = useSortable({
+    id,
+    data: {
+      type: "container",
+      children: items,
+    },
+    animateLayoutChanges: (args) =>
+      defaultAnimateLayoutChanges({ ...args, wasDragging: true }),
+  });
 
-    const isOverContainer = over
-      ? (id === over.id && active?.data.current?.type !== "container") ||
-        items.some(({ id }) => id === over.id)
-      : false;
+  const isOverContainer = over
+    ? (id === over.id && active?.data.current?.type !== "container") ||
+      items.some(({ id }) => id === over.id)
+    : false;
 
-    return (
-      <Column
-        ref={(el) => {
-          if (ref && typeof ref !== "function") {
-            ref.current = el;
-          }
-          setNodeRef(el);
-        }}
-        style={{
-          transition,
-          transform: CSS.Translate.toString(transform),
-          visibility: isDragging ? "hidden" : undefined,
-        }}
-        hover={isOverContainer}
-        handleProps={{
-          ...attributes,
-          ...listeners,
-        }}
-        {...props}
-      >
-        {children}
-      </Column>
-    );
-  },
-);
+  return (
+    <Column
+      ref={(el) => {
+        if (ref && typeof ref !== "function") {
+          ref.current = el;
+        }
+        setNodeRef(el);
+      }}
+      style={{
+        transition,
+        transform: CSS.Translate.toString(transform),
+        visibility: isDragging ? "hidden" : undefined,
+      }}
+      hover={isOverContainer}
+      handleProps={{
+        ...attributes,
+        ...listeners,
+      }}
+      {...props}
+    >
+      {children}
+    </Column>
+  );
+}
+
 DroppableColumn.displayName = "DroppableColumn";
--- apps/trellix-relay/app/components/Handle.tsx
+++ apps/trellix-relay/app/components/Handle.tsx
@@ -1,44 +1,43 @@
 import { cx } from "class-variance-authority";
 import { GripVerticalIcon } from "lucide-react";
-import { HTMLAttributes, forwardRef, useEffect } from "react";
+import { HTMLAttributes, Ref, useEffect } from "react";
 import { useFocusVisible } from "react-aria";
 
 export type HandleProps = {
   dragOverlay?: boolean;
+  ref?: Ref<HTMLButtonElement>;
 } & HTMLAttributes<HTMLButtonElement>;
 
-export const Handle = forwardRef<HTMLButtonElement, HandleProps>(
-  ({ dragOverlay, ...rest }, ref) => {
-    const { isFocusVisible } = useFocusVisible({ isTextInput: true });
+export function Handle({ dragOverlay, ref, ...rest }: HandleProps) {
+  const { isFocusVisible } = useFocusVisible({ isTextInput: true });
 
-    useEffect(() => {
-      if (!dragOverlay || isFocusVisible) {
-        return;
-      }
+  useEffect(() => {
+    if (!dragOverlay || isFocusVisible) {
+      return;
+    }
 
-      document.body.style.cursor = "grabbing";
+    document.body.style.cursor = "grabbing";
 
-      return () => {
-        document.body.style.cursor = "";
-      };
-    }, [dragOverlay, isFocusVisible]);
+    return () => {
+      document.body.style.cursor = "";
+    };
+  }, [dragOverlay, isFocusVisible]);
 
-    return (
-      <button
-        className={cx(
-          `select-none rounded-md px-2 outline-none focus-visible:ring-2
-          focus-visible:ring-sky-500 sm:px-1`,
-          dragOverlay ? "cursor-[inherit]" : "cursor-grab",
-        )}
-        type="button"
-        ref={ref}
-        {...rest}
-        aria-describedby={undefined}
-      >
-        <GripVerticalIcon className="w-4" />
-      </button>
-    );
-  },
-);
+  return (
+    <button
+      className={cx(
+        `select-none rounded-md px-2 outline-none focus-visible:ring-2
+        focus-visible:ring-sky-500 sm:px-1`,
+        dragOverlay ? "cursor-[inherit]" : "cursor-grab",
+      )}
+      type="button"
+      ref={ref}
+      {...rest}
+      aria-describedby={undefined}
+    >
+      <GripVerticalIcon className="w-4" />
+    </button>
+  );
+}
 
 Handle.displayName = "Handle";
