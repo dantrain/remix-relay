3bc3dc2de3ce8aa49728d1de08fb50069a1db604
Dan Train <dan.train@red-badger.com>
2026-01-31 19:52:57 +0000

    fix(trellix-relay): add safe area padding to home page

--- apps/trellix-relay/app/routes/Home.tsx
+++ apps/trellix-relay/app/routes/Home.tsx
@@ -31,7 +31,10 @@ export default function HomePage() {
         sm:pt-[90px]"
     >
       <Deferred fallback={<LoadingScreen />}>
-        <main className="w-full max-w-7xl p-4 pt-5 sm:py-8">
+        <main
+          className="w-full max-w-7xl p-4 pt-5
+            pb-[calc(1rem+env(safe-area-inset-bottom))] sm:py-8"
+        >
           <h2 className="mb-4 text-lg font-bold text-slate-500 uppercase">
             Your Boards
           </h2>
