779580f00a4cdf8d225e9272449d50d0fc8f26a3
Dan Train <dan.train@red-badger.com>
2026-01-31 23:33:51 +0000

    feat(react): add processMultipartResponse utility for incremental delivery

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -2,5 +2,6 @@ export { Deferred } from "./Deferred";
 export { clientLoaderQuery, getClientLoaderQuery } from "./client-loader-query";
 export { RemixRelayProvider } from "./deferred-query-context";
 export { getCachedResponse } from "./get-cached-response";
+export { processMultipartResponse } from "./incremental-response";
 export { metaQuery } from "./meta-query";
 export { useLoaderQuery, useRouteLoaderQuery } from "./useLoaderQuery";
